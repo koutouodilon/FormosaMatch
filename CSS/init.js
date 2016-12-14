381f800595a13d572eb700fa64efb0a1505d1595
Luis Robles <roblescoulter@gmail.com>
2016-12-15 03:32:47 +0800

    Homepage, Sign in and Sign up template finished

--- CSS/init.js
+++ CSS/init.js
@@ -1,8 +1,39 @@
 (function($){
   $(function(){
 
-    $('.button-collapse').sideNav();
+    //$('.button-collapse').sideNav();
     $('.parallax').parallax();
     $('select').material_select();
+    $(".datepicker").pickadate({
+      selectMonths: true,
+      selectYears: 100,
+      max: true,
+    });
+    $('#signin-modal').modal({
+      complete: function(){
+        $("#signin-modal input").val("");
+        $("#signin-modal input").removeClass("invalid");
+      }
+    });
+    $("#sign-up").on("click", function(){
+      $(".sign-up-1").hide("slide", { direction: "left" },400, function(){
+        $(".sign-up-2").show("slide", { direction: "right"},400);
+      });
+    });
+    $("#sign-up-2").on("click", function() {
+      $(".sign-up-2").hide("slide", { direction: "left"},400, function(){
+        $(".sign-up-3").show("slide", {direction: "right"},400);
+      });
+    });
+    $("#back-sign-up-2").on("click", function() {
+      $(".sign-up-3").hide("slide", { direction: "right"},400, function(){
+        $(".sign-up-2").show("slide", {direction: "left"},400);
+      });
+    });
+    $("#back-sign-up-1").on("click", function() {
+      $(".sign-up-2").hide("slide", { direction: "right"},400, function(){
+        $(".sign-up-1").show("slide", {direction: "left"},400);
+      });
+    });
   }); // end of document ready
   })(jQuery); // end of jQuery name space
